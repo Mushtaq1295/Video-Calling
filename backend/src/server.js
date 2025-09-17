44b03f7889922ab67855a1532786bd34280e3412
Mushtaq Ahamad <mohammadmushtaq1295@gmail.com>
2025-09-17 19:51:50 +0530

    feat: add chat routes and controller for generating stream tokens

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ import 'dotenv/config'
 import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth.route.js'
+import userRoutes from './routes/user.route.js'
+import chatRoutes from './routes/chat.route.js'
+
 import { connectDB } from './lib/db.js';
 
 
@@ -15,6 +18,8 @@ app.use(cookieParser()) //to parse the cookies from the request
 app.use(express.urlencoded({ extended: true })); // parse form data
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users',userRoutes);
+app.use('/api/chat',chatRoutes);
 
 app.get("/",(req,res) =>{
     res.send("Hello world! from backend")
