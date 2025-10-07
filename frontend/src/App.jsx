d52030734d009e2b2971aa6c82e4368d392ed43a
Mushtaq Ahamad <mohammadmushtaq1295@gmail.com>
2025-10-07 11:37:58 +0530

    Normal before setting up tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,10 @@ function App() {
 
   return (
     <>
-       <h1 class="text-3xl font-bold underline">
+       {/* <h1 class="text-3xl font-bold underline">
     Hello world!
-  </h1>
+  </h1> */}
+  <h1>Hello</h1>
        
     </>
   )
