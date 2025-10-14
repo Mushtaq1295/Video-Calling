f9a93c9ceb222df73dae292393ca77cbdb19580e
Mushtaq Ahamad <mohammadmushtaq1295@gmail.com>
2025-10-14 22:21:12 +0530

    feat: add routing and new pages for Home, Login, Signup, Notifications, Call, Chat, and Onboarding; include react-hot-toast dependency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from './Pages/HomePage'
+import { SignUpPage } from './Pages/SignUpPage'
+import { NotificationsPage } from './Pages/NotificationsPage'
+import { CallPage } from './Pages/CallPage'
+import { ChatPage } from './Pages/ChatPage'
+import { OnboardingPage } from './Pages/OnboardingPage'
+import { LoginPage } from './Pages/LoginPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     <div className='data-theme="night" class="p-10">'>
-</div>
-       
-    </>
+     <div className="h-screen" data-theme='night'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/notifications' element={<NotificationsPage />} />
+        <Route path='/call' element={<CallPage />} />
+        <Route path='/chat' element={<ChatPage />} />
+        <Route path='/onboarding' element={<OnboardingPage />} />
+      </Routes>
+    </div>
 )
 }
 
